61d8260162d40aa9ea7813c52e236618eefc0b72
RedSkittleFox <redskittlefox@gmail.com>
2023-01-03 03:51:46 +0100

    EASTL fetch content update

--- utilities/fetch_content.cmake
+++ utilities/fetch_content.cmake
@@ -1,32 +1,47 @@
 include(FetchContent)
 
-macro(FetchContent_Declare_URL_Populate LIBRARY_NAME URL)
+macro(FetchContent_Declare_URL_Populate LIBRARY_NAME_M URL)
 
-    if(NOT EXISTS "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME}-src")
+    if(NOT EXISTS "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
         FetchContent_Declare(
-            ${LIBRARY_NAME}
+            ${LIBRARY_NAME_M}
             URL ${URL}
         )
 
-        FetchContent_Populate(${LIBRARY_NAME})
+        FetchContent_Populate(${LIBRARY_NAME_M})
     else()
-        set(${LIBRARY_NAME}_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME}-src")
+        set(${LIBRARY_NAME_M}_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
+    endif()
+endmacro()
+
+macro(FetchContent_Declare_URL_MakeAvailable LIBRARY_NAME_M URL)
+
+    if(NOT EXISTS "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
+        FetchContent_Declare(
+            ${LIBRARY_NAME_M}
+            URL ${URL}
+        )
+
+        FetchContent_MakeAvailable(${LIBRARY_NAME_M})
+    else()
+        set(${LIBRARY_NAME_M}_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
+        add_subdirectory("${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src/")
     endif()
 
 endmacro()
 
-macro(FetchContent_Declare_URL_MakeAvailable LIBRARY_NAME URL)
+macro(FetchContent_Declare_GIT_MakeAvailable LIBRARY_NAME_M URL)
 
-    if(NOT EXISTS "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME}-src")
+    if(NOT EXISTS "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
         FetchContent_Declare(
-            ${LIBRARY_NAME}
-            URL ${URL}
+            ${LIBRARY_NAME_M}
+            GIT_REPOSITORY ${URL}
         )
 
-        FetchContent_MakeAvailable(${LIBRARY_NAME})
+        FetchContent_MakeAvailable(${LIBRARY_NAME_M})
     else()
-        set(${LIBRARY_NAME}_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME}-src")
-        add_subdirectory("${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME}-src/")
+        set(${LIBRARY_NAME_M}_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src")
+        add_subdirectory("${FETCHCONTENT_BASE_DIR}/${LIBRARY_NAME_M}-src/")
     endif()
 
 endmacro()
